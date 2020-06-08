5cde27c7a1d41cf255185bdf71149f88b0b7b8f3
tvoyamamasuka <61188794+tvoyamamasuka@users.noreply.github.com>
2020-06-08 09:39:31 +0300

    Add files via upload

---
+++ File2/File2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace File2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.menuStrip1.Location = new System.Drawing.Point(20, 60);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(337, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\\\";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(20, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(339, 316);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "back and select пути";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 436);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Обозреватель";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ File2/File2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using MetroFramework.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics; 
+using System.IO;
+
+namespace File2
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm //дезигн
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MetroStyleManager metroStyleManager1 = null; //графика метро
+            this.StyleManager = metroStyleManager1; //графика метро
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\') // удаляем хэш
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1); 
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            else if (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+            {
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+        
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // путь дирректории отображается в текстбоксе
+            DirectoryInfo[] dirs = dir.GetDirectories(); // Массив
+            foreach (DirectoryInfo ssDir in dirs)
+            {
+                listBox1.Items.Add(ssDir); 
+            }
+
+            FileInfo[] files = dir.GetFiles(); // отображение файлов
+           
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            {
+                if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "") //открытие файла
+                {
+                    textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+                    listBox1.Items.Clear(); //типа очищает листбокс чтоб файлы новые показать
+                    DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // путь в текстбокс
+                    DirectoryInfo[] dirs = dir.GetDirectories(); // массив
+                    foreach (DirectoryInfo ssDir in dirs)
+                    {
+                        listBox1.Items.Add(ssDir); 
+                    }
+
+                    FileInfo[] files = dir.GetFiles(); 
+                    foreach (FileInfo ssFile in files) // выводим файлы на экран
+                    {
+                        listBox1.Items.Add(ssFile);
+                    }
+                }
+                else
+                {
+                    Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())); // Вывод
+                }
+            }
+        }
+    }
+}
